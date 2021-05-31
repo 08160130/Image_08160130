99d7187942aabc30482835dff17aa81bbdcd5ab2
08160130 <80247079+08160130@users.noreply.github.com>
2021-05-31 16:14:57 +0800

    Update L2_image_info.py

--- L2_image_info.py
+++ L2_image_info.py
@@ -7,7 +7,7 @@ img=cv2.imread(filename,-1)
 nr,nc=img.shape[:2]
 print("Number of Rows=",nr)
 print("Number of Columns=",nc)
-if img.ndim!=3:   """ 維度數3是彩色 """
+if img.ndim!=3:   #維度數3是彩色
     print("Gray-Lavel Image")
-else:   """ 維度數2是灰階 """
+else:   #維度數2是灰階
     print("Color Image")
