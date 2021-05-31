4199f4678b8ceba359e5d58c38a36e5652a8e151
08160130 <80247079+08160130@users.noreply.github.com>
2021-05-31 16:18:18 +0800

    Update L4_01.py

--- L4_01.py
+++ L4_01.py
@@ -1,15 +1,15 @@
 import numpy as np
 import cv2
 
-img1 = cv2.imread( "lena.bmp", 0 )   """讀進一個圖片"""
-nr2, nc2 = img1.shape[:2]   """讀圖片長寬"""
+img1 = cv2.imread( "lena.bmp", 0 )   #讀進一個圖片
+nr2, nc2 = img1.shape[:2]   #讀圖片長寬
 
 
-angle = eval( input( "Please enter angle: " ) )   """請輸入一個角度"""
+angle = eval( input( "Please enter angle: " ) )   #請輸入一個角度
 
 
-rotation_matrix = cv2.getRotationMatrix2D( ( nr2 / 2, nc2 / 2 ), angle, 0.73 )   """((旋轉中心),角度,縮放大小)"""
-img2 = cv2.warpAffine( img1, rotation_matrix, ( nr2, nc2 ) )   """warpAffine仿射轉換函式"""
+rotation_matrix = cv2.getRotationMatrix2D( ( nr2 / 2, nc2 / 2 ), angle, 0.73 )   #((旋轉中心),角度,縮放大小)
+img2 = cv2.warpAffine( img1, rotation_matrix, ( nr2, nc2 ) )   #warpAffine仿射轉換函式
 
 
 
